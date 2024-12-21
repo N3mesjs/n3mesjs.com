d2a196674533067fa0c641e71fc2318c365ba324
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2024-12-21 18:02:07 +0100

    .

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,29 @@
 import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
-      <nav className="flex justify-center text-white mt-2 items-center gap-3 bg-[#272727] rounded-full">
-        <h1 className="text-[20px]">N3mesjs</h1>
-        <a href="home" className="nav-link">Home</a>
+      <div className="absolute flex w-full justify-center top-3">
+          <div className="flex justify-center  items-center bg-[#161616] rounded-full p-[20px]">
+            <a href="/"><h1 className="text-[20px] font-bold mr-5 title rounded-full p-2">N3mesjs</h1></a>
+            <a href="/">
+              <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>Home</span><hr /></div>
+            </a>
+            <a href="/about">
+            <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>About</span><hr /></div>
+            </a>
+            <a href="/project">
+            <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>My Projects</span><hr /></div>
+            </a>
+          </div>
+      </div>
+
+      <nav className="flex items-center justify-end mt-4 mr-4">
+        <a href=""><button className="flex items-center bg-[hsl(212,79%,46%)] font-bold p-3 rounded-2xl gap-2"><FaInstagram size={25} />Instagram</button></a>
       </nav>
+
+      <main></main>
     </>
   );
 }
