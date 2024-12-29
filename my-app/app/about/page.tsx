a6f810ea7128b5c9eac34c969a3dba6a12cd8ef5
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2024-12-29 17:55:54 +0100

    .

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,5 +1,17 @@
-export default function About(){
-    return (
-        <></>
-    );
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+      <h1>Test Links</h1>
+      {/* Link interno */}
+      <Link href="/">
+        home
+      </Link>
+      {/* Link esterno */}
+      <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+        External Link
+      </a>
+    </div>
+  );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,26 +3,32 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-      <div className="absolute flex w-full justify-center top-3">
+    <div>
+      <nav>
+        <div className="absolute flex w-full justify-center top-3">
           <div className="flex justify-center  items-center bg-[#161616] rounded-full p-[10px]">
             <Link href="/"><h1 className="text-[20px] font-bold mr-5 title rounded-full p-2">N3mesjs</h1></Link>
             <Link href="/">
               <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>Home</span><hr /></div>
             </Link>
             <Link href="/about">
-            <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>About</span><hr /></div>
+              <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>About</span><hr /></div>
             </Link>
             <Link href="/project">
-            <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>My Projects</span><hr /></div>
+              <div className="flex flex-col p-3 rounded-full items-center nav-link "><span>My Projects</span><hr /></div>
             </Link>
           </div>
-      </div>
+        </div>
 
-      <nav className="flex items-center justify-end mt-4 mr-4 gap-4">
-        <a href="https://www.instagram.com/delsorbo_alessio/" target="_blank" className="flex items-center bg-[hsl(212,79%,46%)] hover:bg-[hsl(212,54%,28%)] font-bold p-3 rounded-2xl gap-2"><FaInstagram size={25} />Instagram</a>
-        <a href="https://github.com/N3mesjs" target="_blank" className="flex items-center bg-[hsl(212,17%,17%)] hover:bg-[hsl(207,17%,12%)] font-bold p-3 rounded-2xl gap-2"><FaGithub size={25} />Github</a>
+        <div className="flex items-center justify-end mt-4 mr-4 gap-4">
+            <a href="https://www.instagram.com/delsorbo_alessio/" target="_blank" className="flex items-center bg-[hsl(212,79%,46%)] hover:bg-[hsl(212,54%,28%)] font-bold p-3 rounded-2xl gap-2">
+              <FaInstagram size={25} />Instagram
+            </a>
+            <a href="https://github.com/N3mesjs" target="_blank" className="flex items-center bg-[hsl(212,17%,17%)] hover:bg-[hsl(207,17%,12%)] font-bold p-3 rounded-2xl gap-2">
+              <FaGithub size={25} />Github
+            </a>
+        </div>
       </nav>
-    </>
+    </div>
   );
 }
