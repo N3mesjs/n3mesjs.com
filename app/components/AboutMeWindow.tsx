b4e3045c54f4f32d124df2ab7bdb4245fe3c2b6f
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-03-07 21:59:29 +0100

    .

--- app/components/AboutMeWindow.tsx
+++ app/components/AboutMeWindow.tsx
@@ -26,7 +26,7 @@ export default function AboutMeWindow({ setShowState, setHideState, hideState }:
     return (
         <Draggable axis="both" handle=".handle" nodeRef={nodeRef as RefObject<HTMLElement>}>
             <div ref={nodeRef}>
-                <div className={`absolute flex flex-col z-10 left-1/2 top-0 -translate-x-1/2 -translate-y-[150%] p-[4em] bg-[#272727] rounded-2xl ${hideState ? "hidden" : ""}`}>
+                <div className={`absolute flex flex-col z-10 left-1/2 top-0 -translate-x-1/2 -translate-y-[100%] p-[4em] bg-[#272727] rounded-2xl ${hideState ? "hidden" : ""}`}>
                     <div className='absolute flex top-0 left-0 bg-[#000] w-full justify-between p-2 handle cursor-pointer'>
                         <div className='flex items-center gap-1'>
                             <Image src="/txtIcon.png" alt="Logo" width={30} height={30} draggable={false} quality={100} unoptimized={true} />
